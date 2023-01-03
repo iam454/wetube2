e3f368fc285d18a986ed5b2193acee891051c2b8
iam454 <realseogy@gmail.com>
2023-01-03 12:06:57 +0900

    add middleware, morgan

--- src/server.js
+++ src/server.js
@@ -1,22 +1,28 @@
 import express from "express";
+import morgan from "morgan"; // 어떤 method로 어떤 url에 접근하는지 알기 위해 morgan 사용
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
 
 // 홈페이지
 const handleHome = (req, res) => {
   console.log(`Somebody is trying to go home`);
-  res.send("Home Page");
+  return res.send("Home Page");
 };
 
 // 로그인 페이지
 const handleLogin = (req, res) => {
   console.log(`Somebody is trying to login`);
-  res.send("Login Page");
+  return res.send("Login Page");
 };
 
 // 서버 라우트
+// app.use() -> global middleware을 사용하게 함 (모든 라우트에서 이 미들웨어를 사용)
+// global middleware을 통과하고 app.get()을 시도함
+app.use(logger); // global middleware (어떤 method로 어떤 url에 접근하는지 알기 위한 logger)
+
 app.get("/", handleHome);
 app.get("/login", handleLogin);
 
